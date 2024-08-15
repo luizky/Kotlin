4f7ade01fb010946af6734c05cf16988d4730046
luizky <160685655+luizky@users.noreply.github.com>
2024-08-14 21:50:11 -0300

    Create Manipulação de variaveis .kt

---
+++ Manipulação de variaveis .kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+    var nome: String = "Luiz"
+    var idade: Int = 22
+    var altura: Double = 1.71
+    var estudante: Boolean = true
+
+    var idade1: Int = 22
+    var idade2: Int = 10
+
+
+    var totalidade = idade1 + idade2
+    var totalaltura = altura + altura
+    println("Olá $nome")
+
+    println("Nome da pessoa: $nome")
+    println("Idade da pessoa: $idade")
+    println("Altura da pessoa: $altura")
+
+    println("Soma de duas idades: $totalidade")
+    println("Altura dobrada: $totalaltura")
+    println("Estudante: $estudante")
+    println("vlw pelos dados meu fi")
+
+
+
+}
