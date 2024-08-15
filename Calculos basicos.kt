3c80f0090fe0addeaf51cb84cb8b7f94a93c9b80
luizky <160685655+luizky@users.noreply.github.com>
2024-08-14 22:11:10 -0300

    Create Calculos basicos.kt

---
+++ Calculos basicos.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var nome: String = "Luiz"
+    var idade: Int = 19
+    var sal: Double = 2000.00
+    var meses: Int = 12
+    var produtos: Int = 14
+
+    var salbru = sal * meses
+    var saldes = salbru - 200
+    println("Nome $nome, idade $idade, salario bruto $sal, salario total em meses trabalhados $salbru, salario final $saldes meses trabalhados $meses, e os produtos comprados $produtos  ")
+
+
+
+
+}
